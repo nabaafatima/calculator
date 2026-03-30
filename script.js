f0b47e4ebaaa40f934fbddd693eac135c5dc7765
nabaafatima <nabaafatima7097@gmail.com>
2026-03-30 11:36:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const display = document.getElementById('display');
+function appendValue(value) {
+    if(display.innerText==="0") {
+        display.innerText = value;
+    }
+    else {
+        display.innerText += value;
+    };
+}
+function clearDisplay() {
+    display.innerText = "0";
+};
+function calculate() {
+    try {
+    display.innerText = eval(display.innerText);
+    }
+    catch(error) {
+        display.innerText = "E.R.R.O.R";
+    };
+};
